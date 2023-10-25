5b69238e315dfe4a82fd034df7c7c4fdbc78cf03
Franco La Mattina <fr4ncolamattina@gmail.com>
2023-10-25 19:51:55 -0300

    Change in slidere dimensions

--- src/app/components/Carousel/SplideCarousel.js
+++ src/app/components/Carousel/SplideCarousel.js
@@ -4,6 +4,16 @@ import { CarouselContext } from '@/app/contexts/CarouselContext'
 import { useContext, useEffect, useState } from 'react'
 
 const SplideCarousel = props => {
+  const setVH = () => {
+    return window.innerHeight * 0.01
+  }
+
+  console.log(setVH() * 50, 'vh')
+
+  const vh = setVH()
+
+  const [isMobile, setIsMobile] = useState(false)
+
   const { changeSlide } = useContext(CarouselContext)
 
   useEffect(() => {
@@ -29,13 +39,34 @@ const SplideCarousel = props => {
   return (
     <Splide
       ref={props.splideRef}
-      className="absolute inset-0 xl:mx-auto xl:w-[55rem] 2xl:w-[67rem]"
+      className="absolute inset-0 xl:mx-auto w-full border-2 border-blue-300"
       options={{
         rewind: true,
         gap: '1rem',
         arrows: false,
         perPage: 1,
         type: 'fade',
+        width: `${vh * 110}px`,
+        breakpoints: {
+          640: {
+            width: `${vh * 50}px`,
+          },
+          768: {
+            width: `${vh * 60}px`,
+          },
+          1024: {
+            width: `${vh * 10}px`,
+          },
+          1280: {
+            width: `${vh * 107}px`,
+          },
+          1536: {
+            width: `${vh * 110}px`,
+          },
+          2000: {
+            width: `${vh * 125}px`,
+          },
+        },
       }}
       aria-label="My Favorite Images"
     >
