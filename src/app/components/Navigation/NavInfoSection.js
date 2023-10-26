8715b46cdd414659464cba6bee9afcfa880cdbbe
Franco La Mattina <fr4ncolamattina@gmail.com>
2023-10-26 10:15:50 -0300

    Mobile fix css

--- src/app/components/Navigation/NavInfoSection.js
+++ src/app/components/Navigation/NavInfoSection.js
@@ -5,7 +5,7 @@ const NavInfoSection = props => {
         props.isInfoActive
           ? 'opacity-100 translate-x-none'
           : 'translate-x-8 opacity-0 pointer-events-none'
-      } fixed lg:px-[40px] lg:py-[33px] lg:w-max right-0 top-20 lg:h-full bg-white flex flex-col transition duration-200 ease-in-out`}
+      } fixed lg:px-[40px] lg:py-[33px] lg:w-max right-4 lg:right-0 top-20 lg:h-full bg-white flex flex-col transition duration-200 ease-in-out`}
       style={{ zIndex: 2000 }}
     >
       <p className="cursor-pointer hover:font-bold">
@@ -14,7 +14,7 @@ const NavInfoSection = props => {
 
       <div className="w-12 mt-2 border-t border-gray-400"></div>
 
-      <p className="mt-2 text-xs text-gray-400">
+      <p className="mt-2 text-gray-400">
         Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.
       </p>
     </div>
