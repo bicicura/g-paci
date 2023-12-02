2ab5795af328407665c348655bad6e58ff27b5b1
Franco La Mattina <fr4ncolamattina@gmail.com>
2023-12-02 12:56:59 -0300

    Created Interactive Image component

--- src/app/(LayoutOne)/page.js
+++ src/app/(LayoutOne)/page.js
@@ -1,11 +1,13 @@
 'use client'
 
 import Carousel from '../components/Carousel/Carousel'
+import InteractiveImage from '../components/InteractiveImage.js'
 
 export default function Home() {
   return (
     <main className="text-sm">
-      <Carousel />
+      {/* <Carousel /> */}
+      <InteractiveImage imageUrl="/path/to/your/image.jpg" />
     </main>
   )
 }
---
+++ src/app/components/InteractiveImage.js
@@ -0,0 +1,10 @@
+const InteractiveImage = () => {
+  return (
+    <section
+      style={{ zIndex: 1999 }}
+      className="inset-0 fixed z-50 min-h-screen w-full bg-blue-300"
+    ></section>
+  )
+}
+
+export default InteractiveImage
