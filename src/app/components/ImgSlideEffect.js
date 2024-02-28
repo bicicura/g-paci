b19527c8cedf6069f0fb10b25c2a722415e213c1
Franco La Mattina <fr4ncolamattina@gmail.com>
2024-02-28 16:37:35 -0300

    image-slide-effect: new imgs added, one is shown randomly and main fn is throttled

--- src/app/components/ImgSlideEffect.js
+++ src/app/components/ImgSlideEffect.js
@@ -1,10 +1,27 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 const ImgSlideEffect = () => {
   // porcentaje de en que posición esta el mouse en el eje X del contenedor padre
   const [maskWidth, setMaskWidth] = useState(0)
   const [mousePosition, setMousePosition] = useState(0)
+  const imgs = [
+    { img: 'h-1.jpg', client: 'LOfficiel', id: 0 },
+    { img: 'h-2.jpg', client: 'KOSTUME', id: 1 },
+    { img: 'h-3.jpg', client: 'IMAN', id: 2 },
+    { img: 'h-4.jpg', client: 'Lorem', id: 3 },
+    { img: 'h-5.jpg', client: 'asd', id: 4 },
+  ]
+
+  const [randomImg, setRandomImg] = useState({ img: '', client: '' })
+
+  const generateRandomNumber = () => {
+    const maxNumber = imgs.length
+    setRandomImg(prev => {
+      // Returns a random integer from 0 to maxNumber -1:
+      return imgs[Math.floor(Math.random() * maxNumber)]
+    })
+  }
 
   const calculateMaskWidth = e => {
     const divBounds = e.currentTarget.getBoundingClientRect()
@@ -34,10 +51,27 @@ const ImgSlideEffect = () => {
     setMousePosition(Math.min(Math.max(mousePercentage, 0), 100))
   }
 
-  const handleMouseMove = e => {
-    calculateMaskWidth(e)
-    calculateMousePosition(e)
+  const throttle = (func, limit) => {
+    let inThrottle
+    return function () {
+      const args = arguments
+      const context = this
+      if (!inThrottle) {
+        func.apply(context, args)
+        inThrottle = true
+        setTimeout(() => (inThrottle = false), limit)
+      }
+    }
   }
+
+  const handleMouseMove = useCallback(
+    throttle(e => {
+      calculateMaskWidth(e)
+      calculateMousePosition(e)
+      generateRandomNumber()
+    }, 25),
+    []
+  )
 
   return (
     <div className="min-h-screen flex justify-center items-center">
@@ -48,16 +82,17 @@ const ImgSlideEffect = () => {
         <div className="absolute left-0 right-0 -top-12 w-max mx-auto flex gap-8">
           <span>maskWidth: {Math.floor(maskWidth)}%</span>
           <span>mousePosition: {Math.floor(mousePosition)}%</span>
+          <span>randomImg: {randomImg.img}</span>
         </div>
         <div
           style={{ transform: 'translateX(100%)' }}
           className="absolute -right-4 top-0 w-max mx-auto flex flex-col gap-y-1"
         >
           <span className="text-slate-400 text-sm leading-none">client</span>
-          <span className="font-bold text-lg leading-none">Officiel</span>
+          <span className="font-bold text-lg leading-none">{randomImg.client}</span>
         </div>
         <Image
-          src={`/images/overlap-effect/positive.jpg`}
+          src={`/images/img-slide-effect/h-1.jpg`}
           fill
           className="w-full h-full object-cover"
           sizes="100vw"
@@ -68,17 +103,21 @@ const ImgSlideEffect = () => {
             width: `${maskWidth}%`,
             left: `${mousePosition}%`,
             transform: `translateX(-50%)`,
-            // transition: 'left 1s ease-out',
+            // transition: 'all 90ms ease-in-out',
           }}
           className="w-full h-full absolute"
         >
-          <Image
-            src={`/images/overlap-effect/negative.jpg`}
-            fill
-            className="w-full h-full object-cover"
-            sizes="100vw"
-            alt="Slide img"
-          />
+          {imgs.map(({ img, id }, index) => (
+            <Image
+              key={img}
+              src={`/images/img-slide-effect/${img}`}
+              fill
+              className="w-full h-full object-cover"
+              sizes="100vw"
+              alt="Slide img"
+              style={{ opacity: index === randomImg.id ? 1 : 0 }}
+            />
+          ))}
         </div>
       </div>
     </div>
